9a8e1dea1652cb09c553f2ae8bd02e408c64e2ed
MayankSrivastav <mayanksri18@yahoo.in>
2017-05-09 19:28:38 +0530

    written App component

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+var App = React.createClass({
+  displayName: 'App',
+  getInitialState: function getInitialState() {
+    return {
+      person: {
+        name: 'Mayank Srivastav',
+        bio: 'Software Engineer based in India'
+      },
+      image: 'http://static1.squarespace.com/static/55acc005e4b098e615cd80e2/t/57b057398419c2c454f09924/1471025851733/',
+      quote: {
+        content: 'Beautiful things don\'t ask for attention',
+        source: 'The Secret Life of Walter Mitty'
+      }
+    };
+  },
+  render: function render() {
+    return React.createElement(
+      'div',
+      { className: 'App' },
+      React.createElement(Image, { src: this.state.image }),
+      React.createElement(Profile, { person: this.state.person, quote: this.state.quote })
+    );
+  }
+});
